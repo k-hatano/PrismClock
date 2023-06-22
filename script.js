908366d5914b8ffd3f266f11d8a4499e1a1bdfa5
k-hatano <kent.ruffle.mgj626@gmail.com>
2023-06-22 12:40:05 +0900

    Adjust view, supported resizing by touch or dragging events

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ var gShowGrid = true;
 var gFlipped = true;
 var gShowDate = true;
 var gShowSeconds = true;
+var gLastMouseEvent = undefined;
+var gMouseMoved = false;
+var gLastTouchEvent = undefined;
 
 onload = _ => {
   initialize();
+  windowResized();
   drawGrid();
 };
 
@@ -20,7 +24,7 @@ function drawGrid() {
   var ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, width, height);
 
-  var dy = gridSize / 2;
+  var dy = 0;
   while (dy <= height / 2) {
     ctx.beginPath();
     ctx.lineWidth = 2;
@@ -29,12 +33,14 @@ function drawGrid() {
     ctx.lineTo(width, (height / 2) - dy);
     ctx.stroke();
     
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "#303030";
-    ctx.moveTo(0, (height / 2) + dy);
-    ctx.lineTo(width, (height / 2) + dy);
-    ctx.stroke();
+    if (dy > 0) {
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "#303030";
+      ctx.moveTo(0, (height / 2) + dy);
+      ctx.lineTo(width, (height / 2) + dy);
+      ctx.stroke();
+    }
 
     dy += gridSize;
   }
@@ -64,15 +70,26 @@ function drawGrid() {
 
 function initialize() {
   window.addEventListener('resize', windowResized);
+  document.addEventListener('keydown', mainContainerKeyPressed);
   document.getElementById('settings').addEventListener('click', preventBackgroundClick);
+  document.getElementById('settings').addEventListener('mousedown', preventBackgroundClick);
+  document.getElementById('settings').addEventListener('mousemove', preventBackgroundClick);
+  document.getElementById('settings').addEventListener('mouseup', preventBackgroundClick);
+  document.getElementById('settings').addEventListener('touchstart', preventBackgroundClick);
+  document.getElementById('settings').addEventListener('touchmove', preventBackgroundClick);
   document.getElementById('size').addEventListener('input', sizeChanged);
   document.getElementById('size_value').addEventListener('click', sizeValueClicked);
   document.getElementById('show_grid').addEventListener('change', showGridChanged);
   document.getElementById('flipped').addEventListener('change', flippedChanged);
   document.getElementById('font_family').addEventListener('change', fontFamilyChanged);
-  document.getElementById('show_date').addEventListener('change', showDateChanged);
-  document.getElementById('show_seconds').addEventListener('change', showSecondsChanged);
-  document.getElementById('main_container').addEventListener('click', mainContainerClicked);
+  // document.getElementById('show_date').addEventListener('change', showDateChanged);
+  // document.getElementById('show_seconds').addEventListener('change', showSecondsChanged);
+  document.getElementById('digits').addEventListener('change', digitsChanged);
+  document.getElementById('main_container').addEventListener('mousedown', mainContainerPressed);
+  document.getElementById('main_container').addEventListener('mousemove', mainContainerMouseMoved);
+  document.getElementById('main_container').addEventListener('mouseup', mainContainerLeft);
+  document.getElementById('main_container').addEventListener('touchstart', mainContainerTouchStarted);
+  document.getElementById('main_container').addEventListener('touchmove', mainContainerTouchMoved);
   document.getElementById('main_container').addEventListener('wheel', scrolledByWheel);
   document.getElementById('enter_into_fullscreen').addEventListener('click', enterIntoFullscreenClicked);
   if (!document.fullscreenEnabled) {
@@ -81,7 +98,77 @@ function initialize() {
   heartbeat();
 }
 
+function mainContainerPressed() {
+  gLastMouseEvent = event;
+  gMouseMoved = false;
+}
+
+function mainContainerMouseMoved() {
+  if (gLastMouseEvent != undefined) {
+    var deltaY = event.clientY - gLastMouseEvent.clientY;
+    if (deltaY <= -16) {
+      gSize += 2;
+      document.getElementById('size').value = gSize;
+      updateSize(gSize);
+      gLastMouseEvent = event;
+      gMouseMoved = true;
+    } else if (deltaY >= 16) {
+      gSize -= 2;
+      document.getElementById('size').value = gSize;
+      updateSize(gSize);
+      gLastMouseEvent = event;
+      gMouseMoved = true;
+    }
+  }
+}
+
+function mainContainerLeft() {
+  gLastMouseEvent = undefined;
+  if (!gMouseMoved) {
+    var nowClass = document.getElementById('settings').className;
+    if (nowClass.indexOf("hidden") >= 0) {
+      nowClass = document.getElementById('settings').className = "settings";
+    } else {
+      nowClass = document.getElementById('settings').className = "settings hidden";
+    }
+  }
+}
+
+function mainContainerTouchStarted() {
+  gLastTouchEvent = event;
+}
+
+function mainContainerTouchMoved() {
+  if (gLastTouchEvent != undefined && gLastTouchEvent.targetTouches.length > 0) {
+    var deltaY = event.targetTouches[0].clientY - gLastTouchEvent.targetTouches[0].clientY;
+    if (deltaY <= -16) {
+      gSize += 2;
+      document.getElementById('size').value = gSize;
+      updateSize(gSize);
+      gLastTouchEvent = event;
+      gMouseMoved = true;
+    } else if (deltaY >= 16) {
+      gSize -= 2;
+      document.getElementById('size').value = gSize;
+      updateSize(gSize);
+      gLastTouchEvent = event;
+      gMouseMoved = true;
+    }
+  }
+}
+
 function windowResized() {
+  var eSize = document.getElementById('size');
+  if (gShowSeconds) {
+    eSize.setAttribute('max', Math.floor(document.body.clientWidth / 6));
+  } else {
+    eSize.setAttribute('max', Math.floor(document.body.clientWidth / 4));
+  }
+  if (parseInt(eSize.value) > parseInt(eSize.getAttribute('max'))) {
+    eSize.value = eSize.getAttribute('max');
+    document.getElementById('size').value = gSize;
+  }
+  updateSize(eSize.value);
   drawGrid();
 }
 
@@ -127,17 +214,71 @@ function heartbeat() {
 
 function scrolledByWheel(event) {
   if (event.deltaY > 0) {
-    if (gSize < 255) {
-      gSize += 2;
-      document.getElementById('size').value = gSize;
-      updateSize(gSize);
-    }
+    gSize += 2;
+    document.getElementById('size').value = gSize;
+    updateSize(gSize);
   } else if (event.deltaY < 0) {
-    if (gSize > 13) {
-      gSize -= 2;
-      document.getElementById('size').value = gSize;
-      updateSize(gSize);
+    gSize -= 2;
+    document.getElementById('size').value = gSize;
+    updateSize(gSize);
+  }
+}
+
+function mainContainerKeyPressed() {
+  var keyCode = event.keyCode;
+  if (keyCode == 38 || keyCode == 107 || keyCode == 187 || keyCode == 228) { // 上、プラス、ゲームパッド12
+    gSize += 2;
+    document.getElementById('size').value = gSize;
+    updateSize(gSize);
+  } else if (keyCode == 40 || keyCode == 109 || keyCode == 189 || keyCode == 227) { // 下、マイナス、ゲームパッド11
+    gSize -= 2;
+    document.getElementById('size').value = gSize;
+    updateSize(gSize);
+  } else if (keyCode == 49 || keyCode == 97) { // 1
+    document.getElementById('digits').value = '12';
+    gShowDate = true;
+    gShowSeconds = true;
+    updateDateClockClassName();
+    drawGrid();
+  } else if (keyCode == 50 || keyCode == 98) { // 2
+    document.getElementById('digits').value = '8';
+    gShowDate = true;
+    gShowSeconds = false;
+    updateDateClockClassName();
+    drawGrid();
+  } else if (keyCode == 51 || keyCode == 99) { // 3
+    document.getElementById('digits').value = '6';
+    gShowDate = false;
+    gShowSeconds = true;
+    updateDateClockClassName();
+    drawGrid();
+  } else if (keyCode == 52 || keyCode == 100) { // 4
+    document.getElementById('digits').value = '4';
+    gShowDate = false;
+    gShowSeconds = false;
+    updateDateClockClassName();
+    drawGrid();
+  } else if (keyCode == 83) { // S
+    var nowClass = document.getElementById('settings').className;
+    if (nowClass.indexOf("hidden") >= 0) {
+      nowClass = document.getElementById('settings').className = "settings";
+    } else {
+      nowClass = document.getElementById('settings').className = "settings hidden";
     }
+  } else if (keyCode == 70) { // F
+    gFlipped = !document.getElementById('flipped').checked;
+    document.getElementById('flipped').checked = gFlipped;
+    updateDateClockClassName();
+    drawGrid();
+  } else if (keyCode == 71) { // G
+    gShowGrid = !document.getElementById('show_grid').checked;
+    document.getElementById('show_grid').checked = gShowGrid;
+    if (gShowGrid) {
+      document.getElementById('background_grid').className = "";
+    } else {
+      document.getElementById('background_grid').className = "hidden";
+    }
+    drawGrid();
   }
 }
 
@@ -153,14 +294,6 @@ function sizeValueClicked(event) {
 function sizeChanged(event) {
   updateSize(event.target.value);
 
-  if (gShowSeconds && gSize * 6 > document.body.clientWidth) {
-    document.getElementById('message').innerText = "*Caution* Too large";
-  } else if (!gShowSeconds && gSize * 4 > document.body.clientWidth) {
-    document.getElementById('message').innerText = "*Caution* Too large";
-  } else {
-    document.getElementById('message').innerText = "";
-  }
-
   event.preventDefault();
 
   return false;
@@ -168,6 +301,12 @@ function sizeChanged(event) {
 
 function updateSize(newSize) {
   gSize = parseFloat(newSize);
+  if (gSize < parseFloat(document.getElementById('size').getAttribute('min'))) {
+    gSize = parseFloat(document.getElementById('size').getAttribute('min'));
+  } else if (gSize > parseFloat(document.getElementById('size').getAttribute('max'))) {
+    gSize = parseFloat(document.getElementById('size').getAttribute('max'));
+  }
+
   var clockDigits = document.getElementsByClassName('clock_digit');
   for (object of clockDigits) {
     object.style.width = gSize;
@@ -191,13 +330,6 @@ function updateSize(newSize) {
   }
   document.getElementById('size_value').innerText = gSize;
 
-  if (gShowSeconds && gSize * 6 > document.body.clientWidth) {
-    document.getElementById('message').innerText = "*Caution* Too large";
-  } else if (!gShowSeconds && gSize * 4 > document.body.clientWidth) {
-    document.getElementById('message').innerText = "*Caution* Too large";
-  } else {
-    document.getElementById('message').innerText = "";
-  }
   drawGrid();
   updateDateClockClassName();
 }
@@ -209,47 +341,13 @@ function showDateChanged(event) {
 
 function showSecondsChanged(event) {
   gShowSeconds = event.target.checked;
-  if (gShowSeconds) {
-    document.getElementById('year_1').className = "date_digit";
-    document.getElementById('year_2').className = "date_digit";
-    document.getElementById('second_1').className = "clock_digit";
-    document.getElementById('second_2').className = "clock_digit";
-    document.getElementById('clock').style.width = gSize * 6;
-    document.getElementById('date').style.width = gSize * 6;
-  } else {
-    document.getElementById('year_1').className = "date_digit hidden";
-    document.getElementById('year_2').className = "date_digit hidden";
-    document.getElementById('second_1').className = "clock_digit hidden";
-    document.getElementById('second_2').className = "clock_digit hidden";
-    document.getElementById('clock').style.width = gSize * 4;
-    document.getElementById('date').style.width = gSize * 4;
-  }
   updateDateClockClassName();
-
-  if (gShowSeconds && gSize * 6 > document.body.clientWidth) {
-    document.getElementById('message').innerText = "*Caution* Too large";
-  } else if (!gShowSeconds && gSize * 4 > document.body.clientWidth) {
-    document.getElementById('message').innerText = "*Caution* Too large";
-  } else {
-    document.getElementById('message').innerText = "";
-  }
 
   drawGrid();
 }
 
 function preventBackgroundClick(event) {
   event.stopPropagation();
-  return false;
-}
-
-function mainContainerClicked() {
-  var nowClass = document.getElementById('settings').className;
-  if (nowClass.indexOf("hidden") >= 0) {
-    nowClass = document.getElementById('settings').className = "settings";
-  } else {
-    nowClass = document.getElementById('settings').className = "settings hidden";
-  }
-  event.preventDefault();
   return false;
 }
 
@@ -288,6 +386,30 @@ function fontFamilyChanged() {
   document.getElementById('clock').style.fontFamily = event.target.value;
 }
 
+function digitsChanged() {
+  var newValue = parseInt(event.target.value);
+  switch (newValue) {
+  case 12:
+    gShowDate = true;
+    gShowSeconds = true;
+    break;
+  case 8:
+    gShowDate = true;
+    gShowSeconds = false;
+    break;
+  case 6:
+    gShowDate = false;
+    gShowSeconds = true;
+    break;
+  case 4:
+    gShowDate = false;
+    gShowSeconds = false;
+    break;
+  }
+  updateDateClockClassName();
+  drawGrid();
+}
+
 function updateDateClockClassName() {
   var dateClassName = "date";
   var clockClassName = "clock";
@@ -301,9 +423,27 @@ function updateDateClockClassName() {
   document.getElementById('date').className = dateClassName;
   document.getElementById('clock').className = clockClassName;
 
-  if (gFlipped) {
-    document.getElementById('date').style.transform = "translatey(" + gSize + "px) scale(1, -1)";
+  if (gShowSeconds) {
+    document.getElementById('year_1').className = "date_digit";
+    document.getElementById('year_2').className = "date_digit";
+    document.getElementById('second_1').className = "clock_digit";
+    document.getElementById('second_2').className = "clock_digit";
+    document.getElementById('clock').style.width = gSize * 6;
+    document.getElementById('date').style.width = gSize * 6;
   } else {
-    document.getElementById('date').style.transform = "translatey(" + (-gSize) + "px)";
+    document.getElementById('year_1').className = "date_digit hidden";
+    document.getElementById('year_2').className = "date_digit hidden";
+    document.getElementById('second_1').className = "clock_digit hidden";
+    document.getElementById('second_2').className = "clock_digit hidden";
+    document.getElementById('clock').style.width = gSize * 4;
+    document.getElementById('date').style.width = gSize * 4;
+  }
+
+  if (gFlipped) {
+    document.getElementById('date').style.transform = "translatey(" + (gSize / 2) + "px) scale(1, -1)";
+    document.getElementById('clock').style.transform = "translatey(" + (-gSize / 2) + "px) scale(1, -1)";
+  } else {
+    document.getElementById('date').style.transform = "translatey(" + (-gSize / 2) + "px)";
+    document.getElementById('clock').style.transform = "translatey(" + (gSize / 2) + "px)";
   }
 }
