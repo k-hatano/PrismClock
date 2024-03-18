ef8ff850bfde759b3f004e27271a6f76d788da60
k-hatano <kent.ruffle.mgj626@gmail.com>
2024-03-18 15:31:24 +0900

    ソースコード整理

--- script.js
+++ script.js
@@ -82,8 +82,6 @@ function initialize() {
   document.getElementById('show_grid').addEventListener('change', showGridChanged);
   document.getElementById('flipped').addEventListener('change', flippedChanged);
   document.getElementById('font_family').addEventListener('change', fontFamilyChanged);
-  // document.getElementById('show_date').addEventListener('change', showDateChanged);
-  // document.getElementById('show_seconds').addEventListener('change', showSecondsChanged);
   document.getElementById('digits').addEventListener('change', digitsChanged);
   document.getElementById('main_container').addEventListener('mousedown', mainContainerPressed);
   document.getElementById('main_container').addEventListener('mousemove', mainContainerMouseMoved);
@@ -226,44 +224,15 @@ function scrolledByWheel(event) {
 
 function mainContainerKeyPressed() {
   var keyCode = event.keyCode;
-  if (keyCode == 38 || keyCode == 107 || keyCode == 67 || keyCode == 187 || keyCode == 228) { // 上、プラス、C、ゲームパッド90
+  if (keyCode == 38 || keyCode == 107 || keyCode == 67 || keyCode == 187 || keyCode == 49 || keyCode == 97 || keyCode == 228) { // 上、プラス、C、1、ゲームパッド90
     gSize += 2;
     document.getElementById('size').value = gSize;
     updateSize(gSize);
-  } else if (keyCode == 40 || keyCode == 109 || keyCode == 68 || keyCode == 189 || keyCode == 227) { // 下、マイナス、D、ゲームパッド89
+  } else if (keyCode == 40 || keyCode == 109 || keyCode == 68 || keyCode == 189 || keyCode == 50 || keyCode == 98 || keyCode == 227) { // 下、マイナス、D、2、ゲームパッド89
     gSize -= 2;
     document.getElementById('size').value = gSize;
     updateSize(gSize);
-  } else if (keyCode == 49 || keyCode == 97 || keyCode == 71) { // 1、G
-    document.getElementById('digits').value = '12';
-    gShowDate = true;
-    gShowSeconds = true;
-    updateDateClockClassName();
-    drawGrid();
-  } else if (keyCode == 50 || keyCode == 98 || keyCode == 72) { // 2、H
-    document.getElementById('digits').value = '8';
-    gShowDate = true;
-    gShowSeconds = false;
-    updateDateClockClassName();
-    drawGrid();
-  } else if (keyCode == 51 || keyCode == 99 || keyCode == 73) { // 3、I
-    document.getElementById('digits').value = '6';
-    gShowDate = false;
-    gShowSeconds = true;
-    updateDateClockClassName();
-    drawGrid();
-  } else if (keyCode == 52 || keyCode == 100 || keyCode == 74) { // 4、J
-    document.getElementById('digits').value = '4';
-    gShowDate = false;
-    gShowSeconds = false;
-    updateDateClockClassName();
-    drawGrid();
-  } else if (keyCode == 53 || keyCode == 101 || keyCode == 69 || keyCode == 37) { // 5、E、左
-    gFlipped = !document.getElementById('flipped').checked;
-    document.getElementById('flipped').checked = gFlipped;
-    updateDateClockClassName();
-    drawGrid();
-  } else if (keyCode == 54 || keyCode == 101 || keyCode == 70 || keyCode == 39) { // 6、F、右
+  } else if (keyCode == 51 || keyCode == 99 || keyCode == 37) { // 3、左
     gShowGrid = !document.getElementById('show_grid').checked;
     document.getElementById('show_grid').checked = gShowGrid;
     if (gShowGrid) {
@@ -272,20 +241,66 @@ function mainContainerKeyPressed() {
       document.getElementById('background_grid').className = "hidden";
     }
     drawGrid();
-  } else if (keyCode == 55 || keyCode == 67 || keyCode == 78) { // 7、N
+  } else if (keyCode == 52 || keyCode == 100 || keyCode == 39) { // 4、右
+    gFlipped = !document.getElementById('flipped').checked;
+    document.getElementById('flipped').checked = gFlipped;
+    flippedChanged();
+    updateDateClockClassName();
+    drawGrid();
+  } else if (keyCode == 53 || keyCode == 101 || keyCode == 71) { // 5、G
     if (document.getElementById('font_family').selectedIndex >= document.getElementById('font_family').options.length - 1) {
       document.getElementById('font_family').selectedIndex = 0;
     } else {
       document.getElementById('font_family').selectedIndex = document.getElementById('font_family').selectedIndex + 1;
     }
     fontFamilyChanged();
+  } else if (keyCode == 54 || keyCode == 102 || keyCode == 74) { // 6、J
+    if (document.getElementById('font_family').selectedIndex <= 0) {
+      document.getElementById('font_family').selectedIndex = document.getElementById('font_family').options.length - 1;
+    } else {
+      document.getElementById('font_family').selectedIndex = document.getElementById('font_family').selectedIndex - 1;
+    }
+    fontFamilyChanged();
+  } else if (keyCode == 55 || keyCode == 103 || keyCode == 72) { // 7、H
+    if (document.getElementById('digits').selectedIndex >= document.getElementById('digits').options.length - 1) {
+      document.getElementById('digits').selectedIndex = 0;
+    } else {
+      document.getElementById('digits').selectedIndex = document.getElementById('digits').selectedIndex + 1;
+    }
+    digitsChanged();
+    updateDateClockClassName();
+    drawGrid();
+  } else if (keyCode == 56 || keyCode == 104 || keyCode == 73) { // 8、I
+    if (document.getElementById('digits').selectedIndex <= 0) {
+      document.getElementById('digits').selectedIndex = document.getElementById('digits').options.length - 1;
+    } else {
+      document.getElementById('digits').selectedIndex = document.getElementById('digits').selectedIndex - 1;
+    }
+    digitsChanged();
+    updateDateClockClassName();
+    drawGrid();
   } else if (keyCode == 48 || keyCode == 96 || keyCode == 79) { // 0、O
     var nowClass = document.getElementById('settings').className;
     if (nowClass.indexOf("hidden") >= 0) {
-      nowClass = document.getElementById('settings').className = "settings";
+      document.getElementById('settings').className = "settings";
     } else {
-      nowClass = document.getElementById('settings').className = "settings hidden";
+      document.getElementById('settings').className = "settings hidden";
     }
+  } else if (keyCode == 57 || keyCode == 105 || keyCode == 78) { // 9、N
+    gSize = 64;
+    document.getElementById('size').value = gSize;
+    document.getElementById('settings').className = "settings";
+    document.getElementById('show_grid').checked = true;
+    document.getElementById('background_grid').className = ""
+    document.getElementById('flipped').checked = true;
+    document.getElementById('font_family').selectedIndex = 0;
+    document.getElementById('digits').selectedIndex = 0;
+    updateSize(gSize);
+    flippedChanged();
+    fontFamilyChanged();
+    digitsChanged();
+    updateDateClockClassName();
+    drawGrid();
   }
 
   if (keyCode == 13 && document.getElementById('settings').className.indexOf('hidden') >= 0) { // Enterキー、かつ設定フォームが非表示
@@ -303,7 +318,7 @@ function sizeValueClicked(event) {
 }
 
 function sizeChanged(event) {
-  updateSize(event.target.value);
+  updateSize(document.getElementById('size').value);
 
   event.preventDefault();
 
@@ -345,25 +360,13 @@ function updateSize(newSize) {
   updateDateClockClassName();
 }
 
-function showDateChanged(event) {
-  gShowDate = event.target.checked;
-  updateDateClockClassName()
-}
-
-function showSecondsChanged(event) {
-  gShowSeconds = event.target.checked;
-  updateDateClockClassName();
-
-  drawGrid();
-}
-
 function preventBackgroundClick(event) {
   event.stopPropagation();
   return false;
 }
 
 function showGridChanged(event) {
-  gShowGrid = event.target.checked;
+  gShowGrid = document.getElementById('show_grid').checked;
   if (gShowGrid) {
     document.getElementById('background_grid').className = "";
     drawGrid();
@@ -376,9 +379,8 @@ function showGridChanged(event) {
 }
 
 function flippedChanged(event) {
-  gFlipped = event.target.checked;
+  gFlipped = document.getElementById('flipped').checked;
   updateDateClockClassName();
-  event.preventDefault();
 
   drawGrid();
   return false;
@@ -398,7 +400,7 @@ function fontFamilyChanged() {
 }
 
 function digitsChanged() {
-  var newValue = parseInt(event.target.value);
+  var newValue = parseInt(document.getElementById('digits').value);
   switch (newValue) {
   case 12:
     gShowDate = true;
